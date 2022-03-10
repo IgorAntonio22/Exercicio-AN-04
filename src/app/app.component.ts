b892d2449c34b8a7d31d60c6645bc45555f9fb2b
IgorAntonio22 <igorantonio052000@gmail.com>
2022-03-10 01:15:57 -0300

    quinto commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Exercicio4AN';
+
+
+
+  constructor(private _snackBar: MatSnackBar) {}
+
+  mostrarSnackBar(): void {
+    this._snackBar.open('Pacote comprado', 'Fechar')
+  }
 }
